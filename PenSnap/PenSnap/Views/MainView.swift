5ddc7fbc9c55fac6befd69296716d714bb7795c6
deepxshine <aevdok2800@gmail.com>
2024-10-20 00:14:45 +0300

    fix

--- PenSnap/PenSnap/Views/MainView.swift
+++ PenSnap/PenSnap/Views/MainView.swift
@@ -15,21 +15,18 @@ struct MainView: View {
             HomeView()
                 .tabItem {
                     Image(systemName: "house")
-                    Text("Главная")
                 }
                 .tag(0)
             
             SearchView()
                 .tabItem {
                     Image(systemName: "magnifyingglass")
-                    Text("Поиск")
                 }
                 .tag(2)
             
             AddPostView()
                 .tabItem {
                     Image(systemName: "plus.square")
-                    Text("Добавить пост")
                 }
                 .tag(1)
             
@@ -38,7 +35,6 @@ struct MainView: View {
             ProfileView()
                 .tabItem {
                     Image(systemName: "person")
-                    Text("Профиль")
                 }
                 .tag(3)
         }
