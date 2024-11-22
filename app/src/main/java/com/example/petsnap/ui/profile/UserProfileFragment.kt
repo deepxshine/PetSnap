0d74bd126e1aef0a3cf34b56b6a7e0ee100f6a27
apollydolly <polina0106028049@gmail.com>
2024-11-22 22:05:01 +0300

    Deleted the unnecessary viewmodel

--- app/src/main/java/com/example/petsnap/ui/profile/UserProfileFragment.kt
+++ app/src/main/java/com/example/petsnap/ui/profile/UserProfileFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -24,7 +23,7 @@ class UserProfileFragment : Fragment() {
     private var _binding: FragmentUserProfileBinding? = null
     private val binding get() = _binding!!
 
-    private val viewModel: UserProfileViewModel by activityViewModels()
+    private val viewModel: ProfileViewModel by activityViewModels()
     private lateinit var postsAdapter: PostsAdapter
 
     override fun onCreateView(
